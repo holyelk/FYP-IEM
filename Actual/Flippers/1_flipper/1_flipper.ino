389f01c8aa0d9a8473c8bb16fc5b094d95fa46d9
suckms <tanzyb4@yahoo.com.sg>
2020-09-17 22:13:21 +0800

    Added code for 1 flipper with button

---
+++ Actual/Flippers/1_flipper/1_flipper.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int inPin = 7;   // choose the input pin (for a pushbutton)
+int val = 0;     // variable for reading the pin status
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(inPin, INPUT);    // declare pushbutton as input
+}
+
+void loop() {
+  val = digitalRead(inPin);  // read input value
+  if (val == LOW) {  //LOW is button pressed
+    myservo.write(60);
+  } else {
+    myservo.write(0);
+  }
+  
+}
